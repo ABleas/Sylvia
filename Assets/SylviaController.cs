9cc6ffddf08fdedbe452c64d9c3276f9ae41bf1c
Alex Metabi <alex@mekiki.ai>
2023-01-28 12:13:53 +0900

    Controls WIP

--- Assets/SylviaController.cs
+++ Assets/SylviaController.cs
@@ -7,6 +7,7 @@ public class SylviaController : MonoBehaviour
     private Vector2 accel = Vector2.zero;
     public Vector2 vel = Vector2.zero;
     public float speed = 1f;
+    public float decel = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,21 +18,41 @@ public class SylviaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        bool up = Input.GetKeyDown(KeyCode.W);
+        bool down = Input.GetKeyDown(KeyCode.S);
+        bool left = Input.GetKeyDown(KeyCode.A);
+        bool right = Input.GetKeyDown(KeyCode.D);
+
+        if (!up && !down)
+            accel.y *= decel * Time.deltaTime;
+
+        if (!left && !right)
+            accel.x *= decel * Time.deltaTime;
+
+        if (up)
         {
-            accel += Vector2.up * Time.deltaTime;
-        } else if (Input.GetKeyDown(KeyCode.S))
+            accel += Vector2.up * speed * Time.deltaTime;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
         {
-            accel += Vector2.down * Time.deltaTime;
+            accel += Vector2.down * speed * Time.deltaTime;
         }
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            accel += Vector2.left * Time.deltaTime;
+            accel += Vector2.left * speed * Time.deltaTime;
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            accel += Vector2.right * Time.deltaTime;
+            accel += Vector2.right * speed * Time.deltaTime;
         }
+
+        accel = Vector2.ClampMagnitude(accel, 2);
+
+        vel += accel;
+        vel = Vector2.ClampMagnitude(vel, 20);
+
+        transform.position += new Vector3(vel.x, vel.y, 0);
+
     }
 }
